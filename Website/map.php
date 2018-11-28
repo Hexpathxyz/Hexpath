da59ec47d8e106024b78c0807422c96374df670e
edwardsc <codyedwards.1@dcmail.ca>
2018-11-27 21:37:04 -0500

    Added map.php

---
+++ Website/map.php
@@ -0,0 +1,18 @@
+<?php include 'includes/header.php' ?>
+		<h1><span>prologue</span></h1>
+		<div>
+		
+			<div class="article">
+				<h2>World Map</h2>
+
+<img src="images/map.png"alt="Planets" usemap="#map">
+
+<map name="map">
+  <area shape="rect" coords="0,0,82,126" alt="Sun" href="sun.htm">
+  <area shape="circle" coords="90,58,3" alt="Mercury" href="mercur.htm">
+  <area shape="circle" coords="124,58,8" alt="Venus" href="venus.htm">
+</map>
+			</div>
+		</div>
+	</div>
+<?php include 'includes/footer.php' ?>
